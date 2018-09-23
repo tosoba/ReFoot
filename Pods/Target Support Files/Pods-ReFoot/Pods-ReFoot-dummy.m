caef3027d547698ff40088db90ef1030c9e7ba08
merengue <therealmerengue@gmail.com>
2018-09-23 17:53:36 +0200

    Update .gitignore.

--- Pods/Target Support Files/Pods-ReFoot/Pods-ReFoot-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReFoot : NSObject
-@end
-@implementation PodsDummy_Pods_ReFoot
-@end
--- Pods/Target Support Files/ReRxSwift/ReRxSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ReRxSwift : NSObject
-@end
-@implementation PodsDummy_ReRxSwift
-@end
