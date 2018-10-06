14f438b95840c25dc49906ebf6dc2641fbbed9dc
merengue <therealmerengue@gmail.com>
2018-10-06 14:00:43 +0200

    Displaying day events.

---
+++ ReFoot/Scenes/DayEvents/DayEventsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DayEventsTableViewCell.swift
+//  ReFoot
+//
+//  Created by merengue on 06/10/2018.
+//  Copyright © 2018 merengue. All rights reserved.
+//
+
+import UIKit
+
+class DayEventsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ReFoot/Scenes/DayEvents/DayEventsTableViewSection.swift
@@ -0,0 +1,9 @@
+//
+//  DayEventsTableViewSection.swift
+//  ReFoot
+//
+//  Created by merengue on 06/10/2018.
+//  Copyright © 2018 merengue. All rights reserved.
+//
+
+import Foundation
